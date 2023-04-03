59de143215dc9b198dd9038ed0ec6d56218a3840
ArchanaQA23 <126373004+ArchanaQA23@users.noreply.github.com>
2023-04-03 11:40:57 +0530

    Update stepDefs.java

--- src/test/java/parallel/stepDefs.java
+++ src/test/java/parallel/stepDefs.java
@@ -10,6 +10,8 @@ public class stepDefs {
 public void step_from_in_feature_file(String scenario, String file) {
 	    System.out.format("Thread ID - %2d - %s from %s feature file.\n",
 	    		Thread.currentThread().getId(),scenario,file);
+	
+	System.out.println("Hi.. Guys");
 }
 
 }
